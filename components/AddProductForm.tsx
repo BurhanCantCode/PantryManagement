0974e3c35062a3a9e183d25476c833d55fc29cf9
Burhanuddin Khatri <144617735+BurhanCantCode@users.noreply.github.com>
2024-08-03 17:45:42 +0000

    ui changes mhm

--- components/AddProductForm.tsx
+++ components/AddProductForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { TextField, Button, Box, Typography, CircularProgress, Dialog, DialogTitle, DialogContent, DialogActions, Snackbar, Alert } from '@mui/material';
 import { useProducts } from '../hooks/useProducts';
-import ImageCapture from './ImageCapture';
+import ImageCapture from '../components/ImageCapture'; // Corrected import path
 import { scanProduct } from '../utils/visionUtils';
 
 const AddProductForm = () => {
